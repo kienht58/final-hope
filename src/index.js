b8d4387f8bd4b12b094c77e8240586662cfca8de
kienht58 <kien.isp15@gmail.com>
2017-06-28 03:11:51 +0700

    add google analytics

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {Router} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import ReactGA from 'react-ga';
 
 // import generateDatabase from './dbgenerator';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+ReactGA.initialize('UA-101745688-1');
+
+const history = createBrowserHistory();
+history.listen(function(location, action) {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
 // generateDatabase('https://6c7ca13f-773e-463d-9c75-5c714cf8dd87-bluemix.cloudant.com/books')
 ReactDOM.render((
-    <Router>
+    <Router history={history}>
         <App />
     </Router>),
     document.getElementById('root'));
